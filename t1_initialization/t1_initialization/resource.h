a789cc476b6741db64f7ef8cd53669bb32151b0b
Alex Sachuk <alex.sachuk@yandex.ru>
2023-02-07 01:41:49 +0300

    Fix code

--- t1_initialization/t1_initialization/resource.h
+++ t1_initialization/t1_initialization/resource.h
@@ -10,9 +10,8 @@
 #define IDD_ABOUTBOX            103
 #define IDM_ABOUT               104
 #define IDM_EXIT                105
-#define IDI_TUTORIAL1           107
+#define IDI_T1                  107
 #define IDI_SMALL               108
-#define IDC_TUTORIAL1           109
 #define IDC_MYICON              2
 #define IDC_STATIC              -1
 // Next default values for new objects
